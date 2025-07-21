75187468a5276f2a29cab372b24166e763f454ba
vaughandroid <chris.vaughan01@bbc.co.uk>
2025-07-21 18:30:13 +0100

    Script cleanup

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const PROMISE_COUNT = 10_000;
 const TIMEOUT_SHORT_MS = 10_000;
 const TIMEOUT_LONG_MS = 30_000;
 
-let resolvedCount = 0;
-
 function captureSnapshot(number) {
     console.log(`Capturing heap snapshot: ${number}`);
     writeHeapSnapshot(`./${number}.heapsnapshot`);
 }
 
+// Grab an initial heap snapshot, before any promises are created.
 captureSnapshot(1);
+
+let resolvedCount = 0; // Count of how many promises have resolved.
 
 console.log('Creating promises...');
 const promises = Array.from({length: PROMISE_COUNT}, (_, i) => i + 1)
@@ -32,16 +33,21 @@ const promises = Array.from({length: PROMISE_COUNT}, (_, i) => i + 1)
         ]);
     });
 
+// At this point, all promises have been created, but none have resolved yet.
 captureSnapshot(2);
 
-console.log('Waiting for raced promises to resolve...', resolvedCount);
-await Promise.all(promises)
+console.log(`Waiting for raced promises to resolve... (Resolved: ${resolvedCount})`);
+await Promise.all(promises);
+
+// At this point, the short timeout promises have resolved and can be GC'd, but the long timeout ones haven't.
 captureSnapshot(3);
 
-console.log('Waiting for long timeout promises to resolve...', resolvedCount);
+console.log(`Waiting for long timeout promises to resolve... (Resolved: ${resolvedCount})`);
 await new Promise((resolve) => {
     setTimeout(resolve, TIMEOUT_LONG_MS - TIMEOUT_SHORT_MS + 1_000)
 });
+
+// After waiting for the long timeout promises to resolve, they can also be GC'd.
 captureSnapshot(4);
 
-console.log('Done!', resolvedCount);
+console.log(`Done! (Resolved: ${resolvedCount})`);
